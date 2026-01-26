ccb60c43252fce4da6a7befbf58774d826aa68dc
Sergio Sotter <sergio.sotter@network-rg.com.br>
2026-01-26 17:57:20 -0300

    Inserido nome da cidade no Navbar para melhor identificação

--- client/src/components/Brand/Logo.jsx
+++ client/src/components/Brand/Logo.jsx
@@ -1,23 +1,41 @@
 import { FaPaw } from "react-icons/fa6";
 
-export default function Logo() {
+export default function Logo({ subtitle }) {
   return (
-    <div className="flex items-center gap-2 select-none">
-      {/* Ícone */}
-      <FaPaw className="text-[#E58E5E] text-[30px]" />
+    <div className="flex items-center select-none">
+      {/* Logo principal */}
+      <div className="flex items-center gap-2">
+        <FaPaw className="text-[#E58E5E] text-[30px]" />
 
-      {/* Texto */}
-      <span
-        className="
-          text-[23px]
-          font-semibold
-          tracking-wide
-          leading-none
-          text-[#6B3F2B]
-        "
-      >
-        Adota<span className="text-[#E58E5E]">Pet</span>
-      </span>
+        <span
+          className="
+            text-[23px]
+            font-semibold
+            tracking-wide
+            leading-none
+            text-[#6B3F2B]
+          "
+        >
+          Adota<span className="text-[#E58E5E]">Pet</span>
+        </span>
+      </div>
+
+      {/* Região (assinatura) */}
+      {subtitle && (
+        <span
+          className="
+            ml-10
+            text-[15px]
+            text-[#6B3F2B]
+            italic
+            tracking-wide
+            leading-none
+          "
+          style={{ fontFamily: "cursive" }}
+        >
+          {subtitle}
+        </span>
+      )}
     </div>
   );
 }
